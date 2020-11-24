ad71d05774d1e08a7503ca65f779cff093272c66
Vladimir <v.a.evtushenko@yandex.ru>
2020-11-24 14:55:41 +0300

    webpack

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const http= require("http")
 const fs=require("fs")
 const server=http.createServer((req,res)=>{
 console.log(req.url)
-const path =`./`
+const path =`./public`
 try {
     console.log("Попытка")
     body=fs.readFileSync(`${path}${req.url}`)
 }
 catch(err){
-    body=fs.readFileSync(`./index.html`)
+    body=fs.readFileSync(`./public/js/index.html`)
 }
     res.end(body)
 })
