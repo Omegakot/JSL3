e140c29e7aebcc0467a3ca478ee1866e347db180
Vladimir <v.a.evtushenko@yandex.ru>
2020-11-29 10:52:23 +0300

    update2

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path= require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports={
-    entry: './src/fuck.js',
+    entry: './src/shopping.js',
     output:{
         path: path.resolve(__dirname,"public/js"),
         filename:"index.js"
